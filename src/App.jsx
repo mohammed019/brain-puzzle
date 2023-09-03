f45beffff9ac2c787366bb883ee1c095025797fa
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-09-03 16:59:21 +0300

    Start the game automatically and count turns

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,17 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
+
     setCards(shuffledCards);
     setTurns(0);
   };
+
+  // start new game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
@@ -82,6 +90,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
