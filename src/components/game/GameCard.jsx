da8c11d1e4d1ae54ef9a05c915ec2ac745f1e2cd
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-09-08 02:12:00 +0300

    add lazy load image component

--- src/components/game/GameCard.jsx
+++ src/components/game/GameCard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import cover from "../../assets/cover.png";
 import "./GameCard.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const GameCard = ({ card, handleChoice, flipped, disabled }) => {
   const handleClick = () => {
@@ -11,9 +12,10 @@ const GameCard = ({ card, handleChoice, flipped, disabled }) => {
   return (
     <div className="card" key={card.id}>
       <div className={flipped ? "flipped" : ""}>
-        <img
+        <LazyLoadImage
           className="front p-[6px] sm:p-3 bg-accent-700 border-2 border-bg-light-700"
           src={card.img}
+          effect="opacity"
           alt="card front"
         />
         <img
